75165d82161fa5c604d4d4edcb17403435406fe5
Devi Prasad <deviprsd21@gmail.com>
2019-02-11 16:39:02 -0600

    #ref-2112019:438P squashing deploy bugs to heroku

--- bageldrops/settings/prod.py
+++ bageldrops/settings/prod.py
@@ -8,6 +8,8 @@ ALLOWED_HOSTS += [
 
 STATIC_URL = 'assets/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
+
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/bageldrops-frontend/',
