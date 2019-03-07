ca1c840d399bb477cf6047b0a269291841052bfe
Maxx Persin <36431722+maxxpersin@users.noreply.github.com>
2019-03-07 14:01:04 -0600

    added modals for displaying products
    these still need some work but click on Launch demo modal and take a look

--- bageldrops-frontend/src/app/app.module.ts
+++ bageldrops-frontend/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
+import { NgbdModalBasic } from './modal-basics';
 
 @NgModule({
     declarations: [
         AppComponent,
-        DropdownComponent
+        DropdownComponent,
+        NgbdModalBasic
     ],
     imports: [
         NgbModule,
