d83847e7dfc51675752051727e8c72738e819c2d
deviprsd <deviprsd21@gmail.com>
2019-04-01 14:06:26 -0500

    fixed automatic products

--- bageldrops-frontend/src/app/_services/api.service.ts
+++ bageldrops-frontend/src/app/_services/api.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 import { config } from '../_models';
-import { isUndefined } from 'util';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-    private apis: BehaviorSubject<any>;
+    private apis: Observable<any>;
 
     constructor(private http: HttpClient) {
-        this.apis = new BehaviorSubject({});
-        this.http.get<any>(`${config.api}`, {params: new HttpParams().set('format','json')}).subscribe((data) => {
-            this.apis.next(data);
-        })
+        this.apis = this.http.get<any>(`${config.api}`, {params: new HttpParams().set('format', 'json')}).pipe(map(res => res));
     }
 
     get(name: string): Observable<any> {
-        return this.http.get<any>(this.apis[name]);
+        return this.apis.pipe(switchMap((apis) => {
+            return this.http.get<any>(apis[name]);
+        }));
     }
 
     post(name: string, options): Observable<any> {
-        if(isUndefined(this.apis)) return new Observable();
-        return this.http.post<any>(this.apis[name], options);
+        return this.apis.pipe(switchMap((apis) => {
+            return this.http.post<any>(apis[name], options);
+        }));
     }
 }
