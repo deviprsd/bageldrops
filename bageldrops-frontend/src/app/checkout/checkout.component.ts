1526ba9d88f6e43975703b84ba2db2eb8428b387
Maxx Persin <36431722+maxxpersin@users.noreply.github.com>
2019-04-10 11:19:59 -0500

    adding forms

--- bageldrops-frontend/src/app/checkout/checkout.component.ts
+++ bageldrops-frontend/src/app/checkout/checkout.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../_services/authentication.service';
 import { ApiService } from '../_services/api.service';
 import { CartService } from '../_services/cart.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+//import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-checkout',
@@ -10,9 +12,41 @@ import { CartService } from '../_services/cart.service';
 })
 export class CheckoutComponent implements OnInit {
 
+  checkoutForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      addr1: new FormControl('', [Validators.required]),
+      addr2: new FormControl(''),
+      state: new FormControl('', [Validators.required]),
+      zip : new FormControl('', [Validators.required])
+    });
+  //returnUrl: string;
+  submitted = false;
+  error: string;
+
   constructor(public authenticationService: AuthenticationService, public apiService: ApiService, private cartService: CartService) { }
 
   ngOnInit() {
+   // this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+
+        if (this.authenticationService.currentUserValue) {
+            this.checkoutForm.setValue({
+              firstName: `${this.authenticationService.currentUserValue.firstName}`, 
+              lastName: `${this.authenticationService.currentUserValue.lastName}`,
+              addr1: '',
+              addr2: '',
+              state: '',
+              zip: ''
+
+            });
+            console.log(this.checkoutForm);
+        }
+
+        //this.titleService.setTitle('BagelDrops | Log In');
+  }
+
+  onSubmit(){
+    
   }
 
 }
