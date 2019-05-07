e4d27f0bfbc6b38eb56c4962195970976bf8034c
Mitchell Resch <resch.mitchel@uwlax.edu>
2019-05-07 16:08:05 -0500

    fixes taxes
    fixed taxes

--- billing/admin.py
+++ billing/admin.py
@@ -5,5 +5,6 @@ from .models import Billing
 # registers billing and appropriate fields in admin
 @admin.register(Billing)
 class BillingAdmin(admin.ModelAdmin):
-    list_display = ('billing_address', 'delivery_address', 'card_number', 'card_security_code', 'card_exp_date')
+    list_display = ('billing_address', 'delivery_address', 'tax_rate', 'card_number',
+                    'card_security_code', 'card_exp_date')
     readonly_fields = ('crt_date', 'mdf_date')
