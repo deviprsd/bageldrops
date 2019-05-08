b040c8a51c3174cdd2996f32431648cf83a3f70a
Maxx Persin <36431722+maxxpersin@users.noreply.github.com>
2019-05-07 22:47:17 -0500

    style edit

--- bageldrops-frontend/src/app/accounts/accounts.component.ts
+++ bageldrops-frontend/src/app/accounts/accounts.component.ts
@@ -14,24 +14,17 @@ export class AccountsComponent implements OnInit {
   pastCarts = [];
   editName = false;
   editNameForm = new FormGroup({
-    firstName: new FormControl("", [Validators.required]),
-    lastName: new FormControl("", [Validators.required])
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required])
   });
   submitted = false;
 
   public options = new HttpHeaders({
     'Content-Type': 'application/json'
-  }); 
+  });
 
   constructor(public authenticationService: AuthenticationService, public apiService: ApiService, public http: HttpClient) {
-    this.apiService.get('carts').subscribe((carts) => {
-      for (let x in carts) {
-        if (carts[x].cart_state === 'COMPLETED') {
-          this.pastCarts[this.pastCarts.length] = carts[x];
-        }
-        //console.log(carts[x]);
-      }
-    })
   }
 
   //User must be able to edit their profile
@@ -42,38 +35,21 @@ export class AccountsComponent implements OnInit {
   onSubmit() {
     const user = this.authenticationService.currentUserValue;
     this.submitted = true;
-    
+
     if (!this.editNameForm.valid) {
       return;
     }
-    console.log(this.editNameForm.value.firstName);
-    this.apiService.get('customers').subscribe((customers) => {
-      for (let x in customers) {
-        //console.log(user);
-        //console.log(customers[x])
-        if (user.customer_id === customers[x].user) {
 
-        }
-      }
-    });
-    
-    // this.http.patch<any>('localhost:8000/api/v1/customers/1', {user: 1}).subscribe((data) => {
-    //   console.log(data);
-    // });
-    this.apiService.get('customers').subscribe((customers) => {
-      for (let j in customers){
-        if (customers[j].user === 2){
-          var tempcustomer = customers[j];
-          tempcustomer.carts = 12738614;
-          //console.log(tempcustomer);
-
-          this.apiService.patch('customers', user.customer_id, {first_name: `${this.editNameForm.value.firstName}`, last_name: `${this.editNameForm.value.lastName}`}).subscribe((data) => {
-            console.log(data);
-          });
-        }
-      }
-    });
-   
+    this.apiService.patch(
+      'customers',
+      user.customer_id,
+      {
+        first_name: `${this.editNameForm.value.firstName}`,
+        last_name: `${this.editNameForm.value.lastName}`,
+        email: `${this.editNameForm.value.email}`
+      }).subscribe((data) => {
+        console.log(data);
+      });
   }
 
   products() {
